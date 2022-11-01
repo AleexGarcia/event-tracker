ccf5d1348617a0eb06e0e72084bdc0f57769546b
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-11-01 15:50:51 -0300

    .

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -7,17 +7,15 @@ import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
 
-const ListaDeEventos: React.FC<{  
-  aoAlterarStatus: (id: number) => void, 
-  aoDeletarEvento: (id: number) => void, 
-  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoDeletarEvento, aoAlterarStatus, aoFiltroAplicado }) => {
+const ListaDeEventos: React.FC<{    
+  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoFiltroAplicado }) => {
   const eventos = useRecoilValue(listaDeEventosState);
 
   return (<section>
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
     <div className={style.Scroll}>
       {eventos.map(evento => (
-        <Evento aoAlterarStatus={aoAlterarStatus} aoDeletarEvento={aoDeletarEvento} evento={evento} key={evento.id} />
+        <Evento  evento={evento} key={evento.id} />
       ))}
     </div>
   </section>)
